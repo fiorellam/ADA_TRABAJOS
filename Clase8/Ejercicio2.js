f0780d5de627cd318fa06a703c679cb94a87157c
fiorellaroma <fiorellaroma@hotmail.com>
2024-11-12 17:48:52 -0800

    Resolving 2nd exercise

---
+++ Clase8/Ejercicio2.js
@@ -0,0 +1,23 @@
+//Verificar si un número es positivo, negativo o cero
+// En este ejercicio, practicarás el uso de condicionales (if, else). Escribe un
+// programa en JavaScript que:
+
+// Requisitos:
+// • Utiliza una estructura de control de flujo (if, else if, else) para hacer las
+// verificaciones.
+// • Usa parseFloat() para convertir la entrada del usuario a un número.
+
+const prompt = require("prompt-sync")({sigint:true});
+
+// 1. Pida al usuario que ingrese un número.
+let numero = parseFloat(prompt("Ingrese un numero: "));
+
+// 2. Verifique si el número es positivo, negativo o igual a cero.
+// 3. Muestre un mensaje indicando cuál es el caso.
+if(numero > 0){
+    console.log(`El número ${numero} que ingresaste es positivo`)
+} else if (numero == 0) {
+    console.log(`El número ${numero} que ingresaste es igual a cero`);
+} else{
+    console.log(`El número ${numero} que ingresaste es negativo`)
+}
