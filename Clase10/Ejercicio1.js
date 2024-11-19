5b57a4acf6868293bd40db3a99f71ba0870163df
fiorellaroma <fiorellaroma@hotmail.com>
2024-11-19 15:20:24 -0800

    Ejercicio 1

---
+++ Clase10/Ejercicio1.js
@@ -0,0 +1,7 @@
+// Nuestra tarea será declarar dos variables: una con el nombre edad y la
+// otra con el nombre peso. Después deberás asignarle un valor.
+// Pista: Recordemos que para crear una variable debemos utilizar la
+// palabra reservada "let", seguida del nombre con el cual luego le
+// haremos referencia.
+let edad = 26;
+let peso = 45;
