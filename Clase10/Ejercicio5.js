29bc0809bd92ef0d0ddb9031e7580d69770b656b
fiorellaroma <fiorellaroma@hotmail.com>
2024-11-19 17:47:06 -0800

    Ejercicio5

---
+++ Clase10/Ejercicio5.js
@@ -0,0 +1,27 @@
+// Declaración y Asignación de Variables:
+// 1. Declara y asigna un valor numérico a dos variables: primerNumero y segundoNumero.
+// Operaciones Matemáticas:
+// 2. Declara las siguientes variables: resultadoSuma,
+// resultadoResta, resultadoMultiplicacion, resultadoDivision.
+// 3. Asigna a cada una de estas variables el resultado de la suma,
+// resta, multiplicación y división de las variables primerNumero y segundoNumero
+// 1. Utiliza console.log(nombreDeLaVariable) para imprimir cada
+// uno de los resultados y ver cómo se ven los mismos.
+// Pista 1: Recuerda que para crear una variable utilizamos la palabra "let"
+// seguida del nombre al cual vamos a hacer referencia.
+// Pista 2: Para asignar el valor a una variable utilizamos el operador "="
+// seguido del valor que queremos asignarle.
+let primerNumero = 10;
+let segundoNumero = 5;
+
+let resultadoSuma = primerNumero + segundoNumero;
+console.log("Resultado de la suma " , resultadoSuma);
+
+let resultadoResta = primerNumero - segundoNumero;
+console.log("Resultado de la resta " , resultadoResta);
+
+let resultadoMultiplicacion = primerNumero * segundoNumero;
+console.log("Resultado de la multiplicación " , resultadoMultiplicacion);
+
+let resultadoDivision = primerNumero / segundoNumero;
+console.log("Resultado de la división " , resultadoDivision);
