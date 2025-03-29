f9182ebbf0971bdf219e07947995dc6e87d7a8b6
Fiorella <fiorellaroma98@gmail.com>
2025-03-28 21:37:17 -0700

    link referencia

--- Modulo3_TS/Clase1/Ejercicios/e10.ts
+++ Modulo3_TS/Clase1/Ejercicios/e10.ts
@@ -8,6 +8,8 @@
 // Este objeto tiene una estructura implícita que TypeScript hace que el dev deba
 //  respetar, podemos ver que el objeto solo tiene 2 propiedades: nombre de tipo 
 //  string y edad de tipo number, ergo la estructura sería la siguiente:
+
+//Referencia https://dev.to/duxtech/fundamentos-de-typescript-5-objetos-4l71
 let objectNew : {
     nombre: string,
     edad: number,
